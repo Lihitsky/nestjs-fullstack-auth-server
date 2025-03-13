5b615460d8a2ded37f77bdae7fea0314f3a306b5
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2025-03-13 17:58:10 +0200

    add @nestjs/mapped-types package and configure session management with Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { IS_DEV_ENV } from './libs/common/utils/is-dev.util';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { IS_DEV_ENV } from './libs/common/utils/is-dev.util';
       isGlobal: true,
       ignoreEnvFile: !IS_DEV_ENV,
     }),
+    PrismaModule,
 
   ],
 })
