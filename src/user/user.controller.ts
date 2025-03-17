949297e52f0e501f7012752c46622e5ad18f6f5c
Artur <artur.lihitskiy@gmail.com>
2025-03-17 17:43:41 +0200

    add user profile and user ID retrieval endpoints with authorization checks

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,26 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common'
+import { UserRole } from '@prisma/__generated__'
+
+import { Authorization } from '@/auth/decorators/auth.decorators'
+import { Authorized } from '@/auth/decorators/authorized.decorator'
 
 import { UserService } from './user.service'
 
 @Controller('users')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
+
+	@Authorization()
+	@Get('profile')
+	@HttpCode(HttpStatus.OK)
+	public async findProfile(@Authorized('id') userId: string) {
+		return this.userService.findById(userId)
+	}
+
+	@Authorization(UserRole.ADMIN)
+	@Get(':id')
+	@HttpCode(HttpStatus.OK)
+	public async findById(@Param('id') userId: string) {
+		return this.userService.findById(userId)
+	}
 }
