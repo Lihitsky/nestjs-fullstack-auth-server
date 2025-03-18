cd89d6d62db75862367362f79d91d371a8a66d6e
Artur <artur.lihitskiy@gmail.com>
2025-03-18 23:35:28 +0200

    feat: implement OAuth callback and connect endpoints in AuthController, enhance AuthService for profile extraction

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,33 @@
 import {
+	BadRequestException,
 	Body,
 	Controller,
+	Get,
 	HttpCode,
 	HttpStatus,
+	Param,
 	Post,
+	Query,
 	Req,
-	Res
+	Res,
+	UseGuards
 } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Request, Response } from 'express'
 
 import { AuthService } from './auth.service'
 import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
+import { AuthProviderGuard } from './guards/provider.quard'
+import { ProviderService } from './provider/provider.service'
 
 @Controller('auth')
 export class AuthController {
-	public constructor(private readonly authService: AuthService) {}
+	public constructor(
+		private readonly authService: AuthService,
+		private readonly providerService: ProviderService,
+		private readonly configService: ConfigService
+	) {}
 
 	@Post('register')
 	@HttpCode(HttpStatus.OK)
@@ -27,6 +39,38 @@ export class AuthController {
 	@HttpCode(HttpStatus.OK)
 	public async login(@Req() req: Request, @Body() dto: LoginDto) {
 		return this.authService.login(req, dto)
+	}
+
+	@Get('oauth/callback/:provider')
+	@UseGuards(AuthProviderGuard)
+	@HttpCode(HttpStatus.OK)
+	public async callback(
+		@Req() req: Request,
+		@Res({ passthrough: true }) res: Response,
+		@Query('code') code: string,
+		@Param('provider') provider: string
+	) {
+		if (!code) {
+			throw new BadRequestException('Code not found')
+		}
+
+		await this.authService.extractProfileFromCode(req, provider, code)
+
+		return res.redirect(
+			`${this.configService.getOrThrow<string>('ALLOWED_ORIGIN')}/dashboard/settings`
+		)
+	}
+
+	@Get('oauth/connect/:provider')
+	@UseGuards(AuthProviderGuard)
+	@HttpCode(HttpStatus.OK)
+	public async connect(@Param('provider') provider: string) {
+		const providerInstance =
+			this.providerService.findByServiceName(provider)
+
+		return {
+			url: providerInstance.getAuthUrl()
+		}
 	}
 
 	@Post('logout')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,16 +10,20 @@ import { AuthMethod, User } from '@prisma/__generated__'
 import { verify } from 'argon2'
 import { Request, Response } from 'express'
 
+import { PrismaService } from '@/prisma/prisma.service'
 import { UserService } from '@/user/user.service'
 
 import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
+import { ProviderService } from './provider/provider.service'
 
 @Injectable()
 export class AuthService {
 	public constructor(
+		private readonly prismaService: PrismaService,
 		private readonly userService: UserService,
-		private readonly configService: ConfigService
+		private readonly configService: ConfigService,
+		private readonly providerService: ProviderService
 	) {}
 
 	public async register(req: Request, dto: RegisterDto) {
@@ -54,6 +58,55 @@ export class AuthService {
 			throw new UnauthorizedException(
 				'Invalid email address or password. Try again.'
 			)
+		}
+
+		return this.saveSession(req, user)
+	}
+
+	public async extractProfileFromCode(
+		req: Request,
+		provider: string,
+		code: string
+	) {
+		const providerInstance =
+			this.providerService.findByServiceName(provider)
+		const profile = await providerInstance.findUserByCode(code)
+
+		const account = await this.prismaService.account.findFirst({
+			where: {
+				id: profile.id,
+				provider: profile.provider
+			}
+		})
+
+		let user = account?.userId
+			? await this.userService.findById(account.userId)
+			: null
+
+		if (user) {
+			return this.saveSession(req, user)
+		}
+
+		user = await this.userService.create(
+			profile.email,
+			'',
+			profile.name,
+			profile.picture,
+			AuthMethod[profile.provider.toUpperCase()],
+			true
+		)
+
+		if (!account) {
+			await this.prismaService.account.create({
+				data: {
+					userId: user.id,
+					type: 'oauth',
+					provider: profile.provider,
+					accessToken: profile.access_token,
+					refreshToken: profile.refresh_token,
+					expiresAt: profile.expires_at
+				}
+			})
 		}
 
 		return this.saveSession(req, user)
